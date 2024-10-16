c734d927d27aeaffca6630a759614465d67567db
pttsrd <ruslan2kiev@gmail.com>
2024-10-16 16:01:16 +0800

    test: Pass turn right command test cases

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -37,19 +37,18 @@ namespace adas
                     pose.heading = 'S';
                 else if (pose.heading == 'S')
                     pose.heading = 'E';
+            } else if (cmd == 'R') {
+                if (pose.heading == 'E')
+                    pose.heading = 'S';
+                else if (pose.heading == 'S')
+                    pose.heading = 'W';
+                else if (pose.heading == 'W')
+                    pose.heading = 'N';
+                else if (pose.heading == 'N')
+                    pose.heading = 'E';
+            } else {
+                continue;
             }
-            // else if (cmd == 'R') {
-            //     if (pose.heading == 'E')
-            //         pose.heading = 'S';
-            //     else if (pose.heading == 'S')
-            //         pose.heading = 'W';
-            //     else if (pose.heading == 'W')
-            //         pose.heading = 'N';
-            //     else if (pose.heading == 'N')
-            //         pose.heading = 'E';
-            // } else {
-            //     continue;
-            // }
         }
     };
 
