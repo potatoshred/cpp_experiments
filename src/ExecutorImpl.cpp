8961895c88733c07e289adbb52fa76aa979df9c8
pttsrd <ruslan2kiev@gmail.com>
2024-10-16 15:34:31 +0800

    test: Pass turn left command test cases

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -28,17 +28,17 @@ namespace adas
                 } else if (pose.heading == 'S') {
                     --pose.y;
                 }
+            } else if (cmd == 'L') {
+                if (pose.heading == 'E')
+                    pose.heading = 'N';
+                else if (pose.heading == 'N')
+                    pose.heading = 'W';
+                else if (pose.heading == 'W')
+                    pose.heading = 'S';
+                else if (pose.heading == 'S')
+                    pose.heading = 'E';
             }
-            // else if (cmd == 'L') {
-            //     if (pose.heading == 'E')
-            //         pose.heading = 'N';
-            //     else if (pose.heading == 'N')
-            //         pose.heading = 'W';
-            //     else if (pose.heading == 'W')
-            //         pose.heading = 'S';
-            //     else if (pose.heading == 'S')
-            //         pose.heading = 'E';
-            // } else if (cmd == 'R') {
+            // else if (cmd == 'R') {
             //     if (pose.heading == 'E')
             //         pose.heading = 'S';
             //     else if (pose.heading == 'S')
