79307a9815829f74a643ad98ca7c81e242b1847f
pttsrd <ruslan2kiev@gmail.com>
2024-10-16 15:28:23 +0800

    test: Add and Pass all move command test cases

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -29,6 +29,27 @@ namespace adas
                     --pose.y;
                 }
             }
+            // else if (cmd == 'L') {
+            //     if (pose.heading == 'E')
+            //         pose.heading = 'N';
+            //     else if (pose.heading == 'N')
+            //         pose.heading = 'W';
+            //     else if (pose.heading == 'W')
+            //         pose.heading = 'S';
+            //     else if (pose.heading == 'S')
+            //         pose.heading = 'E';
+            // } else if (cmd == 'R') {
+            //     if (pose.heading == 'E')
+            //         pose.heading = 'S';
+            //     else if (pose.heading == 'S')
+            //         pose.heading = 'W';
+            //     else if (pose.heading == 'W')
+            //         pose.heading = 'N';
+            //     else if (pose.heading == 'N')
+            //         pose.heading = 'E';
+            // } else {
+            //     continue;
+            // }
         }
     };
 
--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -10,7 +10,7 @@ namespace adas
         return std::tie(lhs.x, lhs.y, lhs.heading) == std::tie(rhs.x, rhs.y, rhs.heading);
     }
 
-    // 测试用例1
+    // 测试用例1：当未传入指令时，返回原来的姿态
     TEST(ExecutorTest, should_return_init_pose_when_without_command)
     {
         // given 给定测试条件
@@ -24,7 +24,7 @@ namespace adas
         ASSERT_EQ(executor->Query(), target); // 调用重载的operator==
     }
 
-    // 测试用例2
+    // 测试用例2：当未初始化且为传入命令时，返回默认姿态
     TEST(ExecutorTest, should_return_default_pose_when_without_init_and_command)
     {
         // given nothing
@@ -37,7 +37,7 @@ namespace adas
         ASSERT_EQ(executor->Query(), target); // 调用重载的operator==
     }
 
-    // 测试用例3
+    // 测试用例3：移动指令M：当面向E时，x++
     TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
     {
         // given
@@ -46,6 +46,39 @@ namespace adas
         executor->Execute("M");
         // then
         const Pose target = {1, 0, 'E'};
+        ASSERT_EQ(executor->Query(), target);
+    }
+    // 测试用例4：移动指令M：当面向W时，x--
+    TEST(ExecutorTest, should_return_x_minus_1_given_command_is_M_and_facing_is_W)
+    {
+        // given
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
+        // when
+        executor->Execute("M");
+        // then
+        const Pose target = {-1, 0, 'W'};
+        ASSERT_EQ(executor->Query(), target);
+    }
+    // 测试用例5：移动指令M：当面向N时，y++
+    TEST(ExecutorTest, should_return_y_plus_1_given_command_is_M_and_facing_is_N)
+    {
+        // given
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+        // when
+        executor->Execute("M");
+        // then
+        const Pose target = {0, 1, 'N'};
+        ASSERT_EQ(executor->Query(), target);
+    }
+    // 测试用例6：移动指令M：当面向S时，y--
+    TEST(ExecutorTest, should_return_y_minus_1_given_command_is_M_and_facing_is_S)
+    {
+        // given
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
+        // when
+        executor->Execute("M");
+        // then
+        const Pose target = {0, -1, 'S'};
         ASSERT_EQ(executor->Query(), target);
     }
 } // namespace adas
