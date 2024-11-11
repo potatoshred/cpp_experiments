acb157af193dfd494ca587b971991476a3a17630
pttsrd <ruslan2kiev@gmai.com>
2024-11-11 21:02:45 +0800

    feat: Add test cases for TR command

---
+++ tests/ExecutorTurnRoundTest.cpp
@@ -0,0 +1,44 @@
+#include "Executor.hpp"
+#include "PoseEq.hpp"
+#include <gtest/gtest.h>
+
+namespace adas
+{
+
+    // TR
+    TEST(ExecutorTurnRoundTest, should_normal_tr_build_left_forward_left)
+    {
+        // given
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+
+        // when
+        executor->Execute("TR");
+        // then
+        const Pose target({0, 1, 'W'});
+        ASSERT_EQ(executor->Query(), target);
+    }
+    // FTR
+    TEST(ExecutorTurnRoundTest, should_fast_tr_build_forward_left_forward_left)
+    {
+        // given
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+
+        // when
+        executor->Execute("FTR");
+        // then
+        const Pose target({1, 1, 'W'});
+        ASSERT_EQ(executor->Query(), target);
+    }
+    // BTR
+    TEST(ExecutorTurnRoundTest, in_the_B_state_the_reverse_command_will_be_ignored)
+    {
+        // given
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+
+        // when
+        executor->Execute("BTR");
+        // then
+        const Pose target({0, 0, 'E'});
+        ASSERT_EQ(executor->Query(), target);
+    }
+}
